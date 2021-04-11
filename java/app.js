ffd0a1df94fd50ab38c173c85e3c74e68c75349e
mohammad ahmad theeb hayajneh <mohammadhayagneh96@gmail.com>
2021-04-11 14:20:39 +0300

    first q

---
+++ java/app.js
@@ -0,0 +1,34 @@
+const Seattle = {
+    min: 23 ,
+    max: 65 ,
+    Avg: 6.3 ,
+    
+  };
+
+  const Tokyo = {
+    min: 3 ,
+    max: 24 ,
+    Avg: 1.2 ,
+    
+  };
+
+   const Dubai = {
+    min: 11 ,
+    max: 38 ,
+    Avg: 3.7 ,
+    
+  };
+  
+  const Paris = {
+    min: 20 ,
+    max: 38 ,
+    Avg: 2.3 ,
+    
+  };
+  const Lima = {
+    min: 2 ,
+    max: 16 ,
+    Avg: 4.6 ,
+    
+  };
+  
